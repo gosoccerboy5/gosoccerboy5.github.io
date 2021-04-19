8ca6b17ec9810a14f676fb8c17b90bf07a582bbb
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-04-18 20:19:10 -0500

    Create rickroll.js
    allows you to rickroll people in a cool way

---
+++ rickroll.js
@@ -0,0 +1,13 @@
+const rickroll = function (event) { 
+  event.preventDefault(); 
+  document.write("Get 'rolled lol"); 
+  window.setTimeout(function() {
+    document.body.innerHTML = '<iframe width="966" height="543" src="https://www.youtube.com/embed/dQw4w9WgXcQ?&autoplay=1" frameborder="0"></iframe>';
+    console.info("https://scratch.mit.edu/discuss/post/5069102/");
+  }, 500)
+}
+
+const rickrollLinks = document.getElementsByClassName("rickroll");
+for (let i = 0; i < rickrollLinks.length; i++) {
+  rickrollLinks[i].addEventListener("click", rickroll);
+}
