5583b16b5152de1251195c193e6e7467f078b55f
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-04-18 19:56:14 -0500

    Update index.js
    no reason to have div id = "footer" twice in the code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function addFooter () {
   let footer = document.createElement("div");
   footer.id = "footer";
-  footer.innerHTML = '<div id="footer"><a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="https://github.com/favicon.ico"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://avatars.githubusercontent.com/u/82768218" width="30" height="30"></a></div>';
+  footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="https://github.com/favicon.ico"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://avatars.githubusercontent.com/u/82768218" width="30" height="30"></a>';
   document.body.appendChild(footer);
 }
 addFooter();
