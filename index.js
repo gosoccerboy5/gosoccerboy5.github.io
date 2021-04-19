da3f225f6243dc7dfb7c207e1063409634128c6d
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-04-18 20:07:22 -0500

    Update index.js
    adding stylesheet, lets see how this goes

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ function addFooter () {
   footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="https://github.com/favicon.ico"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://avatars.githubusercontent.com/u/82768218" width="30" height="30"></a>';
   document.body.appendChild(footer);
 }
+
+function addStylesheet () {
+  let stylesheet = document.createElement("link");
+  stylesheet.rel = "stylesheet";
+  stylesheet.href = "index.css";
+  document.head.appendChild(stylesheet);
+}
 addFooter();
+addStylesheet();
