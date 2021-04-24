99c5a4d600e5907266160b82e92c820b43f7b7b9
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-04-24 18:14:38 -0500

    update index.js to add titles to footer links

--- resources/index.js
+++ resources/index.js
@@ -5,7 +5,7 @@ function wrapMain() {
 function addFooter () {
   let footer = document.createElement("div");
   footer.id = "footer";
-  footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="https://github.com/favicon.ico"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://avatars.githubusercontent.com/u/82768218" width="30" height="30"></a>';
+  footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="https://github.com/favicon.ico" title = "Source code on Github"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://avatars.githubusercontent.com/u/82768218" width="30" height="30" title = "My Scratch profile"></a>';
   document.body.appendChild(footer);
 }
 
