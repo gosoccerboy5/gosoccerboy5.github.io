e5bf659314b0c5b49210642b8d231f678041ed6a
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-05-04 09:46:45 -0500

    update (remove) this weird spacing stuff

--- resources/index.js
+++ resources/index.js
@@ -9,21 +9,21 @@ function wrapMain() {
   document.body.innerHTML = '<div id = "main">' + document.body.innerHTML + "</div>";
 }
 
-function addFooter () {
+function addFooter() {
   let footer = document.createElement("div");
   footer.id = "footer";
   footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="/resources/GitHub-Mark-64px.png" title = "Source code on Github" width = "30" height = "30"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://cdn2.scratch.mit.edu/get_image/user/13633428_60x60.png" width="30" height="30" title = "My Scratch profile"></a>';
   document.body.appendChild(footer);
 }
 
-function addStylesheet () {
+function addStylesheet() {
   let stylesheet = document.createElement("link");
   stylesheet.rel = "stylesheet";
   stylesheet.href = "https://gosoccerboy5.github.io/resources/stylesheet.css";
   document.head.appendChild(stylesheet);
 }
 
-function addFavicon () {
+function addFavicon() {
   let favicon = document.createElement("link");
   favicon.rel = "icon";
   favicon.href = "https://gosoccerboy5.github.io/resources/favicon.ico";
