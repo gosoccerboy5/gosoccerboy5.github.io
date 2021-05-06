056dab90a3f7f7901830e604a4a6526023f9efb6
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-05-06 14:37:04 -0500

    make it more friendly for local development
    now that I have github desktop, I will locally develop this more

--- resources/index.js
+++ resources/index.js
@@ -1,7 +1,7 @@
 function addHeader() {
   var header = document.createElement("div");
   header.id = "header";
-  header.innerHTML = '<a href="/">Homepage</a>';
+  header.innerHTML = '<a href="./index.html">Homepage</a>';
   document.body.insertBefore(header, document.querySelector("#main"));
 }
 
@@ -12,21 +12,21 @@ function wrapMain() {
 function addFooter() {
   let footer = document.createElement("div");
   footer.id = "footer";
-  footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="/resources/GitHub-Mark-64px.png" title = "Source code on Github" width = "30" height = "30"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://cdn2.scratch.mit.edu/get_image/user/13633428_60x60.png" width="30" height="30" title = "My Scratch profile"></a>';
+  footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="./resources/GitHub-Mark-64px.png" title = "Source code on Github" width = "30" height = "30"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://cdn2.scratch.mit.edu/get_image/user/13633428_60x60.png" width="30" height="30" title = "My Scratch profile"></a>';
   document.body.appendChild(footer);
 }
 
 function addStylesheet() {
   let stylesheet = document.createElement("link");
   stylesheet.rel = "stylesheet";
-  stylesheet.href = "https://gosoccerboy5.github.io/resources/stylesheet.css";
+  stylesheet.href = "./resources/stylesheet.css";
   document.head.appendChild(stylesheet);
 }
 
 function addFavicon() {
   let favicon = document.createElement("link");
   favicon.rel = "icon";
-  favicon.href = "https://gosoccerboy5.github.io/resources/favicon.ico";
+  favicon.href = "./resources/favicon.ico";
   document.head.appendChild(favicon);
 }
 
