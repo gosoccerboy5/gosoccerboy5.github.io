bbbceca66bedbb6ef1e78a9ed0c069662f6dd57e
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-05-19 11:41:31 -0500

    remove dependency on <base> element
    i dont trust <base> elements

--- resources/index.js
+++ resources/index.js
@@ -1,45 +1,45 @@
-function addBase() {
-  let base = document.createElement("base");
-  base.href = "https://gosoccerboy5.github.io";
-  document.head.appendChild(base);
-}
+function reference(link) {
+  function addHeader() {
+    var header = document.createElement("div");
+    header.id = "header";
+    header.innerHTML = '<a href="' + link + '">Homepage</a>';
+    document.body.insertBefore(header, document.querySelector("#main"));
+  }
 
-function addHeader() {
-  var header = document.createElement("div");
-  header.id = "header";
-  header.innerHTML = '<a href="index.html">Homepage</a>';
-  document.body.insertBefore(header, document.querySelector("#main"));
-}
+  function wrapMain() {
+    document.body.innerHTML = '<div id = "main">' + document.body.innerHTML + "</div>";
+  }
 
-function wrapMain() {
-  document.body.innerHTML = '<div id = "main">' + document.body.innerHTML + "</div>";
-}
+  function addFooter() {
+    let footer = document.createElement("div");
+    footer.id = "footer";
+    footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="' + link + 'resources/GitHub-Mark-64px.png" title = "Source code on Github" width = "30" height = "30"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://cdn2.scratch.mit.edu/get_image/user/13633428_60x60.png" width="30" height="30" title = "My Scratch profile"></a>';
+    document.body.appendChild(footer);
+  }
 
-function addFooter() {
-  let footer = document.createElement("div");
-  footer.id = "footer";
-  footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="resources/GitHub-Mark-64px.png" title = "Source code on Github" width = "30" height = "30"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://cdn2.scratch.mit.edu/get_image/user/13633428_60x60.png" width="30" height="30" title = "My Scratch profile"></a>';
-  document.body.appendChild(footer);
-}
+  function addStylesheet() {
+    let stylesheet = document.createElement("link");
+    stylesheet.rel = "stylesheet";
+    stylesheet.href = link + "resources/stylesheet.css";
+    document.head.appendChild(stylesheet);
+  }
 
-function addStylesheet() {
-  let stylesheet = document.createElement("link");
-  stylesheet.rel = "stylesheet";
-  stylesheet.href = "resources/stylesheet.css";
-  document.head.appendChild(stylesheet);
-}
+  function addFavicon() {
+    let favicon = document.createElement("link");
+    favicon.rel = "icon";
+    favicon.href = link + "resources/favicon.ico";
+    document.head.appendChild(favicon);
+  }
 
-function addFavicon() {
-  let favicon = document.createElement("link");
-  favicon.rel = "icon";
-  favicon.href = "resources/favicon.ico";
-  document.head.appendChild(favicon);
-}
+  function addCharEncoding() {
+    document.head.innerHTML += "<meta charset='UTF-8' />";
+  }
 
-addBase();
-wrapMain();
-addFooter();
-addStylesheet();
-addHeader();
-addFavicon();
-// Hey. It's cringey code, but it works.
+  wrapMain();
+  addFooter();
+  addStylesheet();
+  addHeader();
+  addFavicon();
+  addCharEncoding();
+  // Hey. It's cringey code, but it works.
+}
