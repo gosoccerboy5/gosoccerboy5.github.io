ece016a9052879087bf2fd0ced14a641bc324b64
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-05-20 14:14:16 -0500

    4 space indents (#25)
    * Update index.html
    
    * Update rickroll.js
    
    * Update stylesheet.css
    
    * Update index.js
    
    * Update 404.html
    
    * Update index.html
    
    * Update about.html
    
    * Update angry-emails.html
    
    * Update custom-fetch.js
    
    * Update email-responses.json
    
    * Update javascript-ide.html
    
    * Update jokes.html
    
    * Update angry-emails.html
    
    * Update index.js
    
    * Update roadmap.html

--- resources/index.js
+++ resources/index.js
@@ -1,50 +1,51 @@
+// Thank you a lot to @Accio1 on Github for this technique!
 function reference(link) {
-  function addHeader() {
-    var header = document.createElement("div");
-    header.id = "header";
-    header.innerHTML = '<a href="' + link + '">Homepage</a>';
-    document.body.insertBefore(header, document.querySelector("#main"));
-  }
+    function addHeader() {
+        var header = document.createElement("div");
+        header.id = "header";
+        header.innerHTML = '<a href="' + link + '">Homepage</a>';
+        document.body.insertBefore(header, document.querySelector("#main"));
+    }
 
-  function wrapMain() {
-    document.body.innerHTML = '<div id = "main">' + document.body.innerHTML + "</div>";
-  }
+    function wrapMain() {
+        document.body.innerHTML = '<div id = "main">' + document.body.innerHTML + "</div>";
+    }
 
-  function addFooter() {
-    let footer = document.createElement("div");
-    footer.id = "footer";
-    footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="' + link + 'resources/GitHub-Mark-64px.png" title = "Source code on Github" width = "30" height = "30"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://cdn2.scratch.mit.edu/get_image/user/13633428_60x60.png" width="30" height="30" title = "My Scratch profile"></a>';
-    document.body.appendChild(footer);
-  }
+    function addFooter() {
+        let footer = document.createElement("div");
+        footer.id = "footer";
+        footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="' + link + 'resources/GitHub-Mark-64px.png" title = "Source code on Github" width = "30" height = "30"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://cdn2.scratch.mit.edu/get_image/user/13633428_60x60.png" width="30" height="30" title = "My Scratch profile"></a>';
+        document.body.appendChild(footer);
+    }
 
-  function addStylesheet() {
-    let stylesheet = document.createElement("link");
-    stylesheet.rel = "stylesheet";
-    stylesheet.href = link + "resources/stylesheet.css";
-    document.head.appendChild(stylesheet);
-  }
+    function addStylesheet() {
+        let stylesheet = document.createElement("link");
+        stylesheet.rel = "stylesheet";
+        stylesheet.href = link + "resources/stylesheet.css";
+        document.head.appendChild(stylesheet);
+    }
 
-  function addFavicon() {
-    let favicon = document.createElement("link");
-    favicon.rel = "icon";
-    favicon.href = link + "resources/favicon.ico";
-    document.head.appendChild(favicon);
-  }
+    function addFavicon() {
+        let favicon = document.createElement("link");
+        favicon.rel = "icon";
+        favicon.href = link + "resources/favicon.ico";
+        document.head.appendChild(favicon);
+    }
 
-  function addCharEncoding() {
-    document.head.innerHTML += "<meta charset='UTF-8' />";
-  }
+    function addCharEncoding() {
+        document.head.innerHTML += "<meta charset='UTF-8' />";
+    }
 
-  function makeDraggable() {
-    document.querySelector("html").draggable = true; // It's fun to drag the page around
-  }
+    function makeDraggable() {
+        document.querySelector("html").draggable = true; // It's fun to drag the page around
+    }
 
-  wrapMain();
-  addFooter();
-  addStylesheet();
-  addHeader();
-  addFavicon();
-  addCharEncoding();
-  //makeDraggable();
-  // Hey. It's cringey code, but it works.
+    wrapMain();
+    addFooter();
+    addStylesheet();
+    addHeader();
+    addFavicon();
+    addCharEncoding();
+    //makeDraggable();
+    // Hey. It's cringey code, but it works.
 }
--- rickroll/rickroll.js
+++ rickroll/rickroll.js
@@ -1,14 +1,14 @@
 const rickroll = function (event) { 
-  event.preventDefault(); 
-  document.write("Get 'rolled lol"); 
-  window.setTimeout(function() {
-    document.body.innerHTML = '<iframe width="966" height="543" src="https://www.youtube.com/embed/dQw4w9WgXcQ?&autoplay=1" frameborder="0"></iframe>';
-    console.info("https://scratch.mit.edu/discuss/post/5069102/");
-  }, 500)
+    event.preventDefault(); 
+    document.write("Get 'rolled lol"); 
+    window.setTimeout(function() {
+        document.body.innerHTML = '<iframe width="966" height="543" src="https://www.youtube.com/embed/dQw4w9WgXcQ?&autoplay=1" frameborder="0"></iframe>';
+        console.info("https://scratch.mit.edu/discuss/post/5069102/");
+    }, 500);
 }
 
 const rickrollLinks = document.getElementsByClassName("rickroll");
 for (let i = 0; i < rickrollLinks.length; i++) {
-  rickrollLinks[i].addEventListener("click", rickroll);
-  rickrollLinks[i].addEventListener("contextmenu", rickroll);
+    rickrollLinks[i].addEventListener("click", rickroll);
+    rickrollLinks[i].addEventListener("contextmenu", rickroll);
 }
