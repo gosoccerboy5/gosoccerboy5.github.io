3ef1eca2f1113db8e0a592e627be3b107d1c49c3
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-05-21 10:38:38 -0500

    use more descriptive tag names
     - from <div id="header"> to <header id="header">
     - from <div id="main"> to <main id="main">
     - from <div id="footer"> to <footer id="footer">

--- resources/index.js
+++ resources/index.js
@@ -1,18 +1,18 @@
 // Thank you a lot to @Accio1 on Github for this technique!
 function reference(link) {
     function addHeader() {
-        var header = document.createElement("div");
+        var header = document.createElement("header");
         header.id = "header";
         header.innerHTML = '<a href="' + link + '">Homepage</a>';
         document.body.insertBefore(header, document.querySelector("#main"));
     }
 
     function wrapMain() {
-        document.body.innerHTML = '<div id = "main">' + document.body.innerHTML + "</div>";
+        document.body.innerHTML = '<main id = "main">' + document.body.innerHTML + "</main>";
     }
 
     function addFooter() {
-        let footer = document.createElement("div");
+        let footer = document.createElement("footer");
         footer.id = "footer";
         footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="' + link + 'resources/GitHub-Mark-64px.png" title = "Source code on Github" width = "30" height = "30"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://cdn2.scratch.mit.edu/get_image/user/13633428_60x60.png" width="30" height="30" title = "My Scratch profile"></a>';
         document.body.appendChild(footer);
