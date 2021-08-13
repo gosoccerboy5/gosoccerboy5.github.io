ec4777f2dc6b08724a77ddab438ba90e1f247521
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-08-13 09:18:32 -0500

    :pray: :pray:
    (desperately trying to fix a bug where localhost doesnt work for other repositories)
    (my resources/index.js file assumed we could just reference "/", but that doesn't work for other repos, because urls based off of that one wouldn't work, eg /formatting-help/resources/stylesheet.css doesn't exist)

--- resources/index.js
+++ resources/index.js
@@ -1,88 +1,73 @@
-Object.prototype.init = function(config) {
-    const keys = Object.keys(config);
-    for (let i = 0; i < keys.length; i++) {
-        this[keys[i]] = config[keys[i]];
-    }
-    return this;
-};
-// Initialize an object without having to create it and then assigning properties
-
-Object.defineProperty(HTMLElement.prototype, "children", {
-    set: function(val) {
-        val.forEach(value => this.appendChild(value));
-    }
-});
-// Defining a setter for html elements to be able to add children
-
-
 (function() {
-    function addHeader() {
-        let header = document.createElement("header").init({
-            id: "header",
-            children: [
-                document.createElement("a").init({href: "/", innerText: "Homepage"}),
-                document.createElement("a").init({href: "/help/roadmap", innerText: "Roadmap"})
-            ]
-        });
-        document.body.insertBefore(header, document.querySelector("#main"));
+    // Thank you a lot to @Accio1 on Github for this technique!
+    function reference(link) {
+        function addHeader() {
+            var header = document.createElement("header");
+            header.id = "header";
+            header.innerHTML = '<a href="' + link + '">Homepage</a>\n<a href="' + link + 'help/roadmap">Roadmap</a>';
+            document.body.insertBefore(header, document.querySelector("#main"));
+        }
+
+        function wrapMain() {
+            document.body.innerHTML = '<main id="main">' + document.body.innerHTML + "</main>";
+        }
+
+        function addFooter() {
+            let footer = document.createElement("footer");
+            footer.id = "footer";
+            footer.innerHTML = '<a href="https://github.com/gosoccerboy5/gosoccerboy5.github.io"><img src="' + link + 'resources/GitHub-Mark-64px.png" title = "Source code on Github" width = "30" height = "30"></a> <a href="https://scratch.mit.edu/users/gosoccerboy5"><img src="https://cdn2.scratch.mit.edu/get_image/user/13633428_60x60.png" width="30" height="30" title="My Scratch profile"></a>';
+            document.body.appendChild(footer);
+        }
+
+        function addStylesheet() {
+            let stylesheet = document.createElement("link");
+            stylesheet.rel = "stylesheet";
+            stylesheet.href = link + "resources/stylesheet.css";
+            document.head.appendChild(stylesheet);
+        }
+
+        function addFavicon() {
+            let favicon = document.createElement("link");
+            favicon.rel = "icon";
+            favicon.href = link + "resources/favicon.ico";
+            document.head.appendChild(favicon);
+        }
+
+        function addCharEncoding() {
+            let meta = document.createElement("meta");
+            meta.setAttribute("charset", "UTF-8");
+            document.head.appendChild(meta);
+        }
+        addFavicon();
+        wrapMain();
+        addFooter();
+        addStylesheet();
+        addHeader();
+        addCharEncoding();
+        // Hey. It's cringey code, but it works.
     }
 
-    function wrapMain() {
-        document.body.innerHTML = '<main id="main">' + document.body.innerHTML + "</main>";
-    }
+    if (location.hostname === "localhost") {
+        let script;
+        const scripts = document.querySelectorAll("script");
+        for (let _script of scripts) {
+            if (/resources\/index\.js$/.test(_script.src)) {
+                script = _script;
+                break;
+            }
+        }
+        // Really dumb hack to filter the script that is resources/index.js (ie, this file)
+        if (script.getAttribute("ref") === null) {
+            reference("/");
+        } else {
+            reference(script.getAttribute("ref"));
+        }
 
-    function addFooter() {
-        let footer = document.createElement("footer").init({
-            id: "footer",
-            children: [
-                document.createElement("a").init({
-                    href: "https://github.com/gosoccerboy5/gosoccerboy5.github.io",
-                    children: [
-                        document.createElement("img").init({
-                            src: "/resources/GitHub-Mark-64px.png", 
-                            title: "Source code on Github", width: "30", height: "30"
-                        })
-                    ]
-                }),
-                document.createElement("a").init({
-                    href: "https://scratch.mit.edu/users/gosoccerboy5",
-                    children: [
-                        document.createElement("img").init({
-                            src: "https://cdn2.scratch.mit.edu/get_image/user/13633428_60x60.png", 
-                            title: "My Scratch profile", width: "30", height: "30"
-                        })
-                    ]
-                })
-            ]
-        });
-        document.body.appendChild(footer);
-        // Now I see why people use React
+        // All the stupid code above does is it filters out the <script> tag that points to this file
+        // And if it has a `ref` attribute, we will reference that url
+        // Otherwise, we just reference our base url
+    } else if (location.host === "gosoccerboy5.github.io") {
+        reference("https://gosoccerboy5.github.io/"); // All right lets just use the fact that we're on them actual webpage
+        // And move on with our lives
     }
-
-    function addStylesheet() {
-        let stylesheet = document.createElement("link").init({
-            rel: "stylesheet", href: "/resources/stylesheet.css"
-        });
-        document.head.appendChild(stylesheet);
-    }
-
-    function addFavicon() {
-        let favicon = document.createElement("link").init({
-            rel: "icon", href: "/resources/favicon.ico"
-        });
-        document.head.appendChild(favicon);
-    }
-
-    function addCharEncoding() {
-        let meta = document.createElement("meta");
-        meta.setAttribute("charset", "UTF-8");
-        document.head.appendChild(meta);
-    }
-    addFavicon();
-    wrapMain();
-    addFooter();
-    addStylesheet();
-    addHeader();
-    addCharEncoding();
-    // Hey. It's cringey code, but it works.
 })();
